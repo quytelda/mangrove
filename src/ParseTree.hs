91d4a709163ea7676ce502e477ab7335824d312f
Quytelda Kahja <quytelda@tamalin.org>
2026-02-13 20:38:56 -0800

    Remove modules that will need to be rewritten

--- src/ParseTree.hs
+++ src/ParseTree.hs
@@ -8,9 +8,6 @@
 
 module ParseTree
   ( ParseTree(..)
-  , satiate
-  , parseTree
-  , parseArguments
   ) where
 
 import           Control.Applicative
@@ -22,7 +19,6 @@ import           Data.Text            (Text)
 
 import           Resolve
 import           Result
-import           Scheme
 import           Stream
 import           Text
 
@@ -62,19 +58,6 @@ instance Functor p => Alternative (ParseTree p) where
   many = ManyNode False
   some = ManyNode True
 
-instance HasValency p => HasValency (ParseTree p) where
-  valency EmptyNode        = Just 0
-  valency (ValueNode _)    = Just 0
-  valency (ParseNode p)    = valency p
-  valency (ProdNode _ l r) = (+) <$> valency l <*> valency r
-  valency (SumNode l r)    = max <$> valency l <*> valency r
-  valency (ManyNode _ p)   = case valency p of
-                               Just n | n <= 0 -> Just 0
-                               _               -> Nothing
-  -- In the above case of 'ManyNode _ p', a ManyNode can accept an
-  -- arbitrary number of parameters, so the maximum valency is either
-  -- infinite or zero depending on whether the valency of 'p' is zero.
-
 instance Resolve p => Resolve (ParseTree p) where
   resolve EmptyNode          = throwError EmptyError
   resolve (ValueNode value)  = pure value
@@ -94,79 +77,3 @@ instance Resolve p => Resolve (ParseTree p) where
   -- Whether this is the best possible way to handle the situation is
   -- unclear. This avoids infinite loops, but might not be the
   -- expected behavior in some unforseen use-case.
-
-instance Scheme s => Render (ParseTree s r) where
-  -- special cases
-  render (SumNode p (ValueNode _)) = "[" <> render p <> "]"
-
-  render EmptyNode                 = "EMPTY"
-  render (ValueNode _)             = "VALUE"
-  render (ParseNode parser)        = renderParser parser
-  render (ProdNode _ l r)          = render l <> sepProd (Proxy @s) <> render r
-  render (SumNode l r)             = render l <> sepSum (Proxy @s) <> render r
-  render (ManyNode False p)        = "[" <> render p <> "...]"
-  render (ManyNode True p)         = render p <> "..."
-
--- | 'feed' traverses the tree until it activates a parser that
--- consumes input. When a subtree successfully consumes input, it is
--- replaced with an updated subtree and the traversal ceases.
-feed :: Scheme s => ParseTree s r -> StreamParser (Token s) (ParseTree s r)
-feed EmptyNode = empty
-feed (ValueNode _) = empty
-feed (ParseNode parser) = ValueNode <$> activate parser
-feed (ProdNode f l r) =
-  (ProdNode f <$> feed l <*> pure r) <|>
-  (ProdNode f l <$> feed r)
-feed (SumNode l r) = feed l <|> feed r
-feed (ManyNode _ tree) =
-  ProdNode (:)
-  <$> feed tree
-  <*> pure (ManyNode False tree)
-
--- | Repeatedly traverse the tree, each time activating the first
--- parser that can consume available input, until no more input can be
--- consumed.
-satiate :: Scheme s => ParseTree s r -> StreamParser (Token s) (ParseTree s r)
-satiate tree = do
-  result <- optional $ feed tree
-  case result of
-    Just tree' -> satiate tree'
-    Nothing    -> pure tree
-
-parseTree
-  :: Scheme s
-  => ParseTree s r -- ^ Parser expression tree
-  -> StreamHandler (Token s) r a
-  -> [Text]
-  -> a
-parseTree tree handler args =
-  runStreamParser (satiate tree) handler { onSuccess = evaluateResult } state
-  where
-    state = StreamState args [] False
-    evaluateResult _state tree' =
-      case resolve tree' of
-        Right value -> onSuccess handler _state value
-        Left err ->
-          case streamContent _state of
-            (token:_) -> onFailure handler _state $ "unexpected " <> render token
-            _         -> onFailure handler _state $ render err
-
-parseArguments
-  :: Scheme s
-  => ParseTree s r
-  -> [Text]
-  -> Result Builder (r, [Text])
-parseArguments tree =
-  parseTree tree StreamHandler
-  { onSuccess = \state result -> pure (result, streamContent state)
-  , onEmpty = \state -> throwWithContext "empty" (streamContext state)
-  , onFailure = \state err -> throwWithContext err (streamContext state)
-  , onHelpRequest = const HelpRequest
-  }
-  where
-    throwWithContext err contexts =
-      throwError
-      $ mconcat
-      $ List.intersperse ": "
-      $ reverse
-      $ err : map render contexts
