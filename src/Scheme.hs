2dbdb013124d9199790f73fa8c937046098e78f5
Quytelda Kahja <quytelda@tamalin.org>
2026-02-20 20:51:02 -0800

    Add Scheme module with Scheme typeclass

---
+++ src/Scheme.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeFamilies   #-}
+
+module Scheme where
+
+import           Data.Kind
+import           Data.Proxy
+
+import           Stream
+
+-- | A scheme is a system of parsers and tokens. It parses a sequence
+-- of arguments into tokens and values.
+class Scheme (s :: Type -> Type) where
+  -- | A token represents a particular interpretation of an argument
+  -- string under this parsing scheme.
+  data Token s
+
+  -- | 'delimiter' is the character that separates argument strings in
+  -- combined string representation. For example, arguments in the CLI
+  -- command @ls -a -l /var@ are separated by spaces.
+  delimiter :: Proxy s -> Char
+
+  -- | 'activate' tries to run a parser on the current input. If the
+  -- parser doesn't apply, it consumes nothing and returns empty. If
+  -- it does apply, it consumes the relevant input and returns a
+  -- result.
+  activate :: s r -> StreamParser (Token s) r
