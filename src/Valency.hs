c790ea4a3f545a86deaa76f6503f7835102df5ab
Quytelda Kahja <quytelda@tamalin.org>
2026-03-04 23:25:16 -0800

    Add Valency module with Valency typeclass

---
+++ src/Valency.hs
@@ -0,0 +1,17 @@
+module Valency where
+
+-- | Valency represents the maximum number of arguments a parsing
+-- structure can consume.
+--
+-- If the valency of a parser is 'Just n', then it might consume up to
+-- 'n' arguments. If the valency is 'Nothing', it can consume an
+-- arbitrary number of arguments.
+class Valency s where
+  valency :: s r -> Maybe Int
+
+-- | Multary parsers can consume more than one argument.
+multary :: Valency s => s r -> Bool
+multary s =
+  case valency s of
+    Just n  -> n > 1
+    Nothing -> True
