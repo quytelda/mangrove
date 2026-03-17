cb59264de386c88c42827a619b5dc065f3ba445a
Quytelda Kahja <quytelda@tamalin.org>
2026-03-16 21:38:33 -0700

    Implement new parseArguments function for Unix argument parsing

--- src/Mangrove/Unix.hs
+++ src/Mangrove/Unix.hs
@@ -1,14 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
 module Mangrove.Unix where
 
 import           Control.Applicative
-import           Data.List.NonEmpty   (NonEmpty)
-import           Data.Text            (Text)
+import           Data.List.NonEmpty      (NonEmpty)
+import           Data.Text               (Text)
+import qualified Data.Text               as T
+import           System.Environment
+import           System.Exit
+import           System.IO
 
+import           Mangrove.ArgumentParser
 import           Mangrove.ParseTree
-import           Mangrove.Scheme.Sub  (SubScheme)
-import qualified Mangrove.Scheme.Sub  as Sub
-import           Mangrove.Scheme.Unix (UnixScheme)
-import qualified Mangrove.Scheme.Unix as Unix
+import           Mangrove.Scheme.Sub     (SubScheme)
+import qualified Mangrove.Scheme.Sub     as Sub
+import           Mangrove.Scheme.Unix    (UnixScheme)
+import qualified Mangrove.Scheme.Unix    as Unix
+import           Mangrove.Stream
+import           Mangrove.Text
 import           Mangrove.TextParser
 
 type UnixParser = ParseTree UnixScheme
@@ -48,3 +58,36 @@ subparameter = ParseNode . Sub.Parameter
 -- | Define a suboption to a CLI option.
 suboption :: Text -> TextParser a -> ParseTree SubScheme a
 suboption key = ParseNode . Sub.Option key
+
+-- | Parse the command line arguments passed to the program, then
+-- invoke the program's entrypoint with the results of the parsing. If
+-- parsing fails, we instead display an error to stderr and exit.
+-- Alternatively, if help was requested, we abandon parsing and print
+-- the relevant help output to stdout, then exit without indicating an
+-- error.
+parseArguments
+  :: UnixParser r
+  -> Text -- ^ Program Name
+  -> Text -- ^ Program Description
+  -> (r -> IO a) -- ^ Program Entrypoint
+  -> IO a
+parseArguments tree name description action = do
+  args <- map T.pack <$> getArgs
+  case runArgumentParser tree args of
+    Success [] result -> action result
+    Success (token:_) _ -> do
+      hPutBuilder stderr $ "unexpected " <> render token <> "\n"
+      exitFailure
+    Failure contexts err -> do
+      hPutBuilder stderr $ renderError contexts err <> "\n"
+      exitFailure
+    HelpRequest contexts -> do
+      let cmds = [s | Unix.UnixCommand s <- contexts]
+          subIndex = Unix.selectSubtable (reverse cmds) helpIndex
+      putBuilder
+        $ "Usage: " <> render name <> " " <> render tree <> "\n\n"
+        <> render description <> "\n"
+        <> Unix.renderTables subIndex
+      exitSuccess
+  where
+    helpIndex = Unix.collectOptions tree
