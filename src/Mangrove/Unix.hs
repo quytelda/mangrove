96a83f1ac0d10f8b82a9f7f7ffac42b7f0dcfb83
Quytelda Kahja <quytelda@tamalin.org>
2026-03-16 21:42:31 -0700

    Unqualify references to Mangrove.Scheme.Unix in Mangrove.Unix

--- src/Mangrove/Unix.hs
+++ src/Mangrove/Unix.hs
@@ -15,8 +15,7 @@ import           Mangrove.ArgumentParser
 import           Mangrove.ParseTree
 import           Mangrove.Scheme.Sub     (SubScheme)
 import qualified Mangrove.Scheme.Sub     as Sub
-import           Mangrove.Scheme.Unix    (UnixScheme)
-import qualified Mangrove.Scheme.Unix    as Unix
+import           Mangrove.Scheme.Unix
 import           Mangrove.Stream
 import           Mangrove.Text
 import           Mangrove.TextParser
@@ -24,23 +23,23 @@ import           Mangrove.TextParser
 type UnixParser = ParseTree UnixScheme
 
 option
-  :: NonEmpty Unix.Flag
+  :: NonEmpty Flag
   -> Text
   -> ParseTree SubScheme r
   -> ParseTree UnixScheme r
-option flags help = ParseNode . Unix.Option (Unix.OptionInfo flags help)
+option flags help = ParseNode . Option (OptionInfo flags help)
 
 -- | Define a CLI option which takes no parameter and produces a pure value.
 optionPure
-  :: NonEmpty Unix.Flag
+  :: NonEmpty Flag
   -> Text
   -> a
   -> ParseTree UnixScheme a
-optionPure flags help = ParseNode . Unix.Option (Unix.OptionInfo flags help) . pure
+optionPure flags help = ParseNode . Option (OptionInfo flags help) . pure
 
 -- | Define a CLI option which produces 'True' if present and 'False'
 -- otherwise.
-switch :: NonEmpty Unix.Flag -> Text -> ParseTree UnixScheme Bool
+switch :: NonEmpty Flag -> Text -> ParseTree UnixScheme Bool
 switch flags help = optionPure flags help True <|> pure False
 
 -- | Define a CLI subcommand with it's own parsing subtree.
@@ -49,7 +48,7 @@ command
   -> Text
   -> ParseTree UnixScheme r
   -> ParseTree UnixScheme r
-command cmds help = ParseNode . Unix.Command (Unix.CommandInfo cmds help)
+command cmds help = ParseNode . Command (CommandInfo cmds help)
 
 -- | Define a subparameter to a CLI option.
 subparameter :: TextParser a -> ParseTree SubScheme a
@@ -82,12 +81,12 @@ parseArguments tree name description action = do
       hPutBuilder stderr $ renderError contexts err <> "\n"
       exitFailure
     HelpRequest contexts -> do
-      let cmds = [s | Unix.UnixCommand s <- contexts]
-          subIndex = Unix.selectSubtable (reverse cmds) helpIndex
+      let cmds = [s | UnixCommand s <- contexts]
+          subIndex = selectSubtable (reverse cmds) helpIndex
       putBuilder
         $ "Usage: " <> render name <> " " <> render tree <> "\n\n"
         <> render description <> "\n"
-        <> Unix.renderTables subIndex
+        <> renderTables subIndex
       exitSuccess
   where
-    helpIndex = Unix.collectOptions tree
+    helpIndex = collectOptions tree
