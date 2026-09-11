0052e5b1ef03832575830f333e43a05feb82ecc3
Quytelda Kahja <quytelda@tamalin.org>
2026-09-10 20:05:20 -0700

    test: Stream: Rewrite tests as property-based tests
    This change simplifies the test structure considerably. We also add
    property tests for Functor and Monad laws.

--- test/Mangrove/StreamSpec.hs
+++ test/Mangrove/StreamSpec.hs
@@ -4,71 +4,138 @@
 
 module Mangrove.StreamSpec (spec) where
 
-import           Data.Text.Lazy.Builder
+import           Data.Maybe
+import           Data.Text                 (Text)
 import           Test.Hspec
+import           Test.Hspec.QuickCheck
+import           Test.QuickCheck           hiding (Result (..))
+import           Test.QuickCheck.Instances ()
 
 import           Mangrove
 import           Mangrove.Scheme.Unix
 import           Mangrove.Stream
+import           Mangrove.Test.Stream
 
-data StreamResult r
-  = SSuccess r
-  | SEmpty
-  | SFailure Builder
-  | SRequest RequestType
-  deriving (Eq, Show)
+--------------------------------------------------------------------------------
+-- Functor Laws
 
--- | Sink the results of a 'StreamParser' into a data type for easier inspection.
-runStreamParser'
-  :: SupportsResponse s
-  => StreamParser s r
-  -> StreamState s
-  -> (StreamState s, StreamResult r)
-runStreamParser' parser state =
-  runStreamParser parser handler state
+prop_functorIdLaw
+  :: SP_Unix Int
+  -> StreamState UnixScheme
+  -> Bool
+prop_functorIdLaw (SP_Unix m) state =
+  runSPU (fmap id m) state == runSPU m state
+
+prop_functorComLaw
+  :: Fun Int Int
+  -> Fun Int Int
+  -> SP_Unix Int
+  -> StreamState UnixScheme
+  -> Bool
+prop_functorComLaw (Fn f) (Fn g) (SP_Unix m) state =
+  runSPU (fmap (f . g) m) state == runSPU ((fmap f . fmap g) m) state
+
+--------------------------------------------------------------------------------
+-- Monad Laws
+
+prop_monadLeftId
+  :: Int
+  -> Fun Int (SP_Unix Int)
+  -> StreamState UnixScheme
+  -> Bool
+prop_monadLeftId a fn state =
+  runSPU (f a) state == runSPU (return a >>= f) state
   where
-    handler = StreamHandler
-      { onSuccess = \s result -> (s, SSuccess result)
-      , onEmpty = \s -> (s, SEmpty)
-      , onFailure = \s err -> (s, SFailure err)
-      , onRequest = OnRequest $ \s t -> (s, SRequest t)
-      }
+    f = getSPU . applyFun fn
 
-initState_empty :: StreamState s
-initState_empty = StreamState [] [] False
+prop_monadRightId
+  :: SP_Unix Int
+  -> StreamState UnixScheme
+  -> Bool
+prop_monadRightId (SP_Unix m) state =
+  runSPU m state == runSPU (m >>= return) state
 
-initState_singleton :: StreamState s
-initState_singleton = StreamState ["asdf"] [] False
+prop_monadAssoc
+  :: SP_Unix Int
+  -> Fun Int (SP_Unix Int)
+  -> Fun Int (SP_Unix Int)
+  -> StreamState UnixScheme
+  -> Bool
+prop_monadAssoc (SP_Unix m) fn1 fn2 state =
+  runSPU ((m >>= f) >>= g) state == runSPU (m >>= (\x -> f x >>= g)) state
+  where
+    f = getSPU . applyFun fn1
+    g = getSPU . applyFun fn2
+
+--------------------------------------------------------------------------------
+
+prop_peek_preservesState
+  :: StreamState UnixScheme
+  -> Bool
+prop_peek_preservesState state =
+  case runSPU peek state of
+    (_, state') -> state == state'
+
+prop_pop_preservesContext
+  :: StreamState UnixScheme
+  -> Bool
+prop_pop_preservesContext state =
+  case runSPU pop state of
+    (_, state') -> streamContext state == streamContext state'
+
+prop_pop_preservesEscaped
+  :: StreamState UnixScheme
+  -> Bool
+prop_pop_preservesEscaped state =
+  case runSPU pop state of
+    (_, state') -> streamEscaped state == streamEscaped state'
+
+prop_yieldsValueOrEmpty
+  :: StreamParser UnixScheme Text
+  -> StreamState UnixScheme
+  -> Bool
+prop_yieldsValueOrEmpty action state =
+  case runSPU action state of
+    (SPSuccess a, _) -> listToMaybe (streamContent state) == Just a
+    (SPEmpty, _)     -> null $ streamContent state
+    _                -> False
+
+prop_consumesValue
+  :: StreamParser UnixScheme a
+  -> StreamState UnixScheme
+  -> Bool
+prop_consumesValue action state =
+  case runSPU action state of
+    (_, state') -> streamContent state' == drop 1 (streamContent state)
 
 spec :: Spec
 spec = do
-  describe "peek" $ do
-    context "when the stream is empty" $ do
-      let (finalState, result) = runStreamParser' peek (initState_empty @UnixScheme)
-      it "returns empty" $ do
-        result `shouldBe` SEmpty
-      it "preserves the state" $ do
-        initState_empty `shouldBe` finalState
+  describe "Functor instance" $ do
+    prop "satisfies identity law"
+      prop_functorIdLaw
+    prop "satisfies composition law"
+      prop_functorComLaw
 
-    context "when the stream is not empty" $ do
-      let (finalState, result) = runStreamParser' peek (initState_singleton @UnixScheme)
-      it "gets the first item" $ do
-        result `shouldBe` SSuccess "asdf"
-      it "preserves the state" $ do
-        initState_singleton `shouldBe` finalState
+  describe "Monad instance" $ do
+    prop "satisfies left identity law"
+      prop_monadLeftId
+    prop "satisfies right identity law"
+      prop_monadRightId
+    prop "satisfies associativity law"
+      prop_monadAssoc
+
+  describe "peek" $ do
+    prop "preserves the stream state"
+      prop_peek_preservesState
+    prop "yields first value or empty" $
+      prop_yieldsValueOrEmpty peek
 
   describe "pop" $ do
-    context "when the stream is empty" $ do
-      let (finalState, result) = runStreamParser' pop (initState_empty @UnixScheme)
-      it "returns empty" $ do
-        result `shouldBe` SEmpty
-      it "preserves the state" $ do
-        initState_empty `shouldBe` finalState
-
-    context "when the stream is not empty" $ do
-      let (finalState, result) = runStreamParser' pop (initState_singleton @UnixScheme)
-      it "gets the first item without replacement" $ do
-        result `shouldBe` SSuccess "asdf"
-        streamContent finalState `shouldBe` drop 1 (streamContent initState_singleton)
-      it "preserves the context" $ do
-        streamContext initState_singleton `shouldBe` streamContext finalState
+    prop "consumes values" $
+      prop_consumesValue pop
+    prop "yields first value or empty" $
+      prop_yieldsValueOrEmpty pop
+    prop "preserves escaped setting" $
+      prop_pop_preservesEscaped
+    prop "preserves the stream state"
+      prop_pop_preservesContext
